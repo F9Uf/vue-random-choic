66a14b822e05b4b3fe8907fd16284726796dba14
F9Uf <atoseka@gmail.com>
2019-07-20 18:50:37 +0700

    add list and auto focus when click text

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app" class="container">
-    <div class="row header py-4 fixed-top" >
+    <!-- header box -->
+    <div class="row sticky-top my-3" >
       <div class="col-auto mx-auto">
-        <h1 class="display-5"><a class="link-white">สุ่มสิจ๊ะ</a></h1>
+        <h1 class="display-5"><a href="/">สุ่มสิจ๊ะ</a></h1>
       </div>
     </div>
-    <form @submit.prevent="randomChoice">
-      <div class="row justify-content-center" style="margin-top: 150px;">
-        <div class="col-sm-auto my-2 text-center" v-for="(choice,index) in choices" :key="index">
-          <input type="text" placeholder="Enter somthing..."
-            v-model="choice.value" class="flat-input-blue">
-          <button @click="deleteChoice(index)" class="flat-button-blue" type="button">-</button>
-        </div>
+    <!-- random list box -->
+    <div class="row">
+      <ul>
+        <li v-for="(item, i) in list" :key="i">
+          <div v-if="i === selectedIndex">
+            <input type="text"
+              v-model="item.text" 
+              v-focus
+              @blur="unSelected()"
+              @keypress.enter="unSelected()"
+            >
+          </div>
+          <div  v-else>
+            <div @click="selectedList(i)">
+              {{ item.text }}
+            </div>
+            <button @click="deleteList(i)">X</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!-- input box -->
+    <div class="row">
+      <div class="col-auto mx-auto">
+        <form @submit.prevent="addNewList()">
+          <input type="text" v-model="newList">
+        </form>
+        {{ selectedIndex }}
       </div>
-      <div class="row">
-        <div class="col-12 text-center my-2">
-          <button @click="addChoice" class="flat-button-violet" type="button">ADD CHOICE</button>
-          <button @click="clearChoices" class="flat-button-violet flat-sm" type="button">CLEAR ALL</button>
-        </div>
-        <div class="col-12 text-center my-2">
-          <button @click="randomChoice" class="flat-button-pink">RANDOM</button>
-        </div>
-        <div class="col-12 text-center my-2">
-          <h1 class="display-5" v-if="random">{{random}}</h1>
-        </div>
-      </div>
-    </form>
-
+    </div>
+    <!-- footer box -->
     <div class="row text-center my-4">
       <div class="col">
         <small>Power by F9Uf. Code on <a class="small-link-white" href="https://github.com/F9Uf/vue-random-choice" target="_BLANK">Github</a></small>
@@ -36,38 +46,46 @@
 </template>
 
 <script>
-// import Spinner from 'vue-simple-spinner'
+
 export default {
   name: 'app',
   data () {
     return {
-      choices: [
-        {value: ''}
-      ],
-      random: null,
-      loading: false,
+      list: [],
+      newList: '',
+      selectedIndex: null,
     }
   },
   methods: {
-    addChoice () {
-      this.choices.push({value: ''})
+    addNewList() {
+      this.list.push(
+        {
+          text: this.newList,   
+        }
+      );
+      this.newList = '';
     },
-    deleteChoice (index) {
-      this.choices.splice(index, 1)
+
+    deleteList(index) {
+      this.selectedIndex = null;
+      this.list.splice(index,1);
     },
-    randomChoice () {
-      if (this.choices.length < 2) {
-        this.random = 'Can not random'
-      } else {
-        this.random = this.choices[Math.floor(Math.random()*this.choices.length)].value
-      }
+
+    selectedList(index) {
+      this.selectedIndex = index;
     },
-    clearChoices () {
-      this.choices = [{ value: ''}]
+
+    unSelected() {
+      this.selectedIndex = null;
     }
   },
-  components: {
-    // Spinner
+
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus()
+      }
+    }
   }
 }
 </script>
