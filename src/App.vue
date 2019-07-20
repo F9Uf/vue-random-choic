c155a783cf24ebb9b8c6237a798594d3e11e4614
F9Uf <atoseka@gmail.com>
2019-07-20 17:17:47 +0700

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" class="container">
-    <div class="row header py-4 fixed-top" style="background-color: #726cac;">
+    <div class="row header py-4 fixed-top" >
       <div class="col-auto mx-auto">
-        <h1 class="display-5"><a class="link-white" href="http://vue-random-choice.herokuapp.com/">VUE RANDOM</a></h1>
+        <h1 class="display-5"><a class="link-white">สุ่มสิจ๊ะ</a></h1>
       </div>
     </div>
     <form @submit.prevent="randomChoice">
@@ -73,119 +73,5 @@ export default {
 </script>
 
 <style>
-a.small-link-white{
-  color: rgb(191, 224, 255);
-}
-a.link-white{
-  text-decoration: none;
-  color: white;
-  transition: .2s all;
-  text-shadow: 0px 1px 0 #52519d, 0px 2px 0 #52519d, 0px 3px 0 #52519d, 0px 4px 0 #52519d, 0px 4px 0 #52519d,
-  0px 5px 0 #52519d,0px 6px 0 #52519d;
-  position: relative;
-}
-a.link-white:hover{
-  opacity: .7;
-}
-a.link-white:active{
-  text-shadow: none;
-  top: 7px;
-}
-.flat-button-violet.flat-sm {
-  width: 120px;
-}
-.flat-button-violet {
-  transition: .2s all;
-  position: relative;
-  vertical-align: top;
-  width: 150px;
-  height: 35px;
-  padding: 0;
-  font-size: 18px;
-  color: #c8cafc;
-  text-align: center;
-  background: #4e509b;
-  border: 0;
-  border-bottom: 3px solid #353766;
-  cursor: pointer;
-  -webkit-box-shadow: inset 0 -2px #353766;
-  box-shadow: inset 0 -2px #353766;
-}
-.flat-button-violet:active {
-  top: 2px;
-  outline: none;
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
 
-.flat-input-blue {
-  border: #39c1db 2px solid;
-  /* border-bottom: 4px solid #39c1db; */
-  background-color: transparent;
-  color: white;
-  padding: 3px 10px;
-  border-radius: 2px;
-}
-::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-  color: #39c1db;
-  opacity: 0.8; /* Firefox */
-}
-
-:-ms-input-placeholder { /* Internet Explorer 10-11 */
-  color: #39c1db;
-  opacity: 0.8;
-}
-
-::-ms-input-placeholder { /* Microsoft Edge */
-  color: #39c1db;
-  opacity: 0.8;
-}
-
-.flat-button-blue{
-  transition: .2s all;
-  position: relative;
-  vertical-align: top;
-  width: 35px;
-  height: 35px;
-  padding: 0;
-  font-size: 18px;
-  color: #316ea8;
-  text-align: center;
-  background: #39c1db;
-  border: 0;
-  border-bottom: 3px solid #316ea8;
-  cursor: pointer;
-  -webkit-box-shadow: inset 0 -2px #316ea8;
-  box-shadow: inset 0 -2px #316ea8;
-}
-.flat-button-blue:active{
-  top: 2px;
-  outline: none;
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
-
-.flat-button-pink{
-  transition: .2s all;
-  position: relative;
-  vertical-align: top;
-  width: 100px;
-  height: 35px;
-  padding: 0;
-  font-size: 18px;
-  color: #fcd6da;
-  text-align: center;
-  background: #ed71a3;
-  border: 0;
-  border-bottom: 3px solid #d2487c;
-  cursor: pointer;
-  -webkit-box-shadow: inset 0 -2px #d2487c;
-  box-shadow: inset 0 -2px #d2487c;
-}
-.flat-button-pink:active{
-  top: 2px;
-  outline: none;
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
 </style>
