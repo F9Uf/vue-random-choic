43e65290a220ad65af622ac7545f61551bfac697
F9Uf <atoseka@gmail.com>
2019-07-20 20:59:45 +0700

    change div to alert bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app" class="container">
     <!-- header box -->
-    <div class="row sticky-top py-3 align-items-end">
+    <div class="row sticky-top py-3 align-items-end" style="z-index: 9999;">
       <div class="col-auto mx-auto">
         <h1 class="display-5"><a href="/">สุ่มสิจ๊ะ</a></h1>
       </div>
     </div>
     <!-- nav bar -->
-    <div class="row fixed-top py-4">
+    <div class="row fixed-top py-4 ">
       <div class="col-auto py-2 ml-auto mr-5">
         <div class="custom-control custom-switch">
           <input type="checkbox"
@@ -24,30 +24,51 @@
     </div>
     <!-- random list box -->
     <div class="row">
-      <ul>
-        <li v-for="(item, i) in list" :key="i">
-          <div v-if="i === selectedIndex">
-            <input type="text"
+      <div class="col">
+        <!-- list -->
+        <!-- <div v-for="(item, i) in list" :key="i" class="list bg-danger">
+          <div class="list-content" v-if="i === selectedIndex">
+            <input type="text" class="form-control"
               v-model="item.text" 
               v-focus
               @blur="unSelected()"
               @keypress.enter="unSelected()"
             >
           </div>
-          <div  v-else>
+          <div  class="list-content" v-else>
             <div @click="selectedList(i)">
               {{ item.text }}
             </div>
-            <button @click="deleteList(i)">X</button>
+            <button @click="deleteList(i)" class="btn-close">&times;</button>
           </div>
-        </li>
-      </ul>
+        </div> -->
+
+        <div v-for="(item, i) in list" :key="i" class="alert alert-dark alert-dismissible fade show list">
+          <div v-if="i === selectedIndex">
+            <input type="text"
+              class="list-input"
+              v-model="item.text"
+              v-focus
+              @blur="unSelectedx()"
+              @keypress.enter="unSelected()"
+            >
+          </div>
+          <div v-else>
+            <div @click="selectedList(i)">
+              {{ item.text }}
+            </div>
+            <button type="button" class="close" @click="deleteList(i)">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- input box -->
     <div class="row">
       <div class="col-auto mx-auto">
         <form @submit.prevent="addNewList()">
-          <input type="text" v-model="newList">
+          <input type="text" v-model="newList" class="form-control">
         </form>
       </div>
     </div>
@@ -77,7 +98,7 @@ export default {
   name: 'app',
   data () {
     return {
-      list: [],
+      list: [{text:'1'},{text:'2'},{text:'4'},{text: '6'}],
       newList: '',
       selectedIndex: null,
       randomResult: {},
